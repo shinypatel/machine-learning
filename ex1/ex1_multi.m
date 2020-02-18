63a2e396cb7c2d0662820f5f582347ec76f48e26
Shiny Patel <patelshiny44@gmail.com>
2020-02-18 15:35:14 -0600

    completed optional exercises

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -163,7 +163,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = theta' * [1, x]'; % You should change this
 
 
 % ============================================================
